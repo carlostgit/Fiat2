b38e3f3ee3a8270a5935e53e6e8d9aab165263d6
carlostgit <carlos.h.corredera@gmail.com>
2022-10-23 20:58:55 +0200

    Añado proyectos de CodeBlocks en c++ en los que voy a trabajar para la parte de GDNative

---
+++ GDNativeSource/Fiat2GDNativeTester/main.cpp
@@ -0,0 +1,49 @@
+#include "../Market/market.h"
+
+//#include <libloaderapi.h>
+#include <Windows.h>
+#include <tchar.h>
+#include <libloaderapi.h>
+#include <wchar.h>
+
+#include <iostream>
+
+using namespace std;
+
+//tipo del método
+//void DLL_EXPORT SomeFunction(const LPCSTR sometext);
+//DWORD(*Arithmetic)(DWORD, DWORD);
+//void(*SomeFunction)(const LPCSTR sometext);
+typedef void(*SomeF)(const LPCSTR sometext);
+//
+
+int main()
+{
+    //Ejemplo con load-time linking. Es necesario compilar linkando con la
+    //librería estática
+    cout << "Hello world!" << endl;
+    //SomeFunction("Hello from load-time linked dll");
+    //Fin del ejemplo con load-time linking.
+
+    //Ejemplo con run-time inking. Creo que no es necesario compilar linkando
+    //con la librería estática
+    //HMODULE hDll = LoadLibrary(_T("Marketd.dll"));
+    HMODULE hDll = LoadLibrary("Marketd.dll");
+    if (!hDll || hDll == INVALID_HANDLE_VALUE) {
+        cout << "unable to load libraray"<< std::endl;
+        return 1;
+    }
+
+    cout << "Marketd.dll loaded"<< std::endl;
+
+    void(*functionPtr)(const LPCSTR sometext);
+    functionPtr = (SomeF)(GetProcAddress(hDll, "SomeFunction"));
+    //Lo anterior queda más bonito, pero también se puede hacer
+    //como en la siguiente linea, sin usar el typdef
+    //functionPtr = (void(*)(const LPCSTR sometext))(GetProcAddress(hDll, "SomeFunction"));
+
+    functionPtr("Hello from run-time linked dll");
+    //Fin del ejemplo con run-time linking.
+
+    return 0;
+}
---
+++ GDNativeSource/Market/market.h
@@ -0,0 +1,28 @@
+#ifndef __MARKET_H__
+#define __MARKET_H__
+
+#include <windows.h>
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void DLL_EXPORT SomeFunction(const LPCSTR sometext);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MARKET_H__
