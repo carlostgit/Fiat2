3c5583097d85b96ff7802ffbaac2aab9c6839de3
carlostgit <carlos.h.corredera@gmail.com>
2023-07-11 00:08:57 +0200

    Añado el proyecto PriceCalculator a la solución para su librería DLL PriceCalculatorDLL.sln

--- GDNativeSource/PriceCalculatorDLL/PriceCalculatorDLL/PriceCalculatorDLL.cpp
+++ GDNativeSource/PriceCalculatorDLL/PriceCalculatorDLL/PriceCalculatorDLL.cpp
@@ -1,10 +1,17 @@
 #include "pch.h"
 #include "PriceCalculatorDLL.h"
 
+//includes para las llamadas a PriceCalculator
+#include "../../PriceCalculator/src/PriceCalculator.h"
+#include "../../PriceCalculator/src/Tester.h"
+
 long GetTestPriceFromDLL()
 {
 	//TODO
 	//Desde aquí llamar a algún método de PriceCalculator.h
+
+	//LLamada de prueba a Tester de PriceCalculator
+	int Test_SatisfactionCurve();
 
 	return 7;
 }
@@ -14,5 +21,7 @@ long GetTestPriceFromDLL2(long nArg)
 	//TODO
 	//Desde aquí llamar a algún método de PriceCalculator.h
 
-	return 7 + nArg;
+	long nTestPrice = pca::CPriceCalculator::GetTestPrice();
+
+	return 7 + nArg + nTestPrice;
 }
